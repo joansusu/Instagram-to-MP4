0469fc3b9a22e2aaac2d39ae6425bfe42ccd157f
joansusu <joanxu2023@gmail.com>
2025-02-05 21:15:25 +0800

    GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css"
 import { Navigation } from "@/components/navigation"
 import { Footer } from "@/components/footer"
 import type React from "react"
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         <main className="flex-grow">{children}</main>
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-SKHS9RZBGR" />
     </html>
   )
 }
